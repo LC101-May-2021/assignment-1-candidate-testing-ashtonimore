6a78be98c850b89640bb2742e35aed4243b21365
ashtonimore <>
2021-06-03 20:41:28 +0000

    Completed part 2.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,30 +8,36 @@ let candidateName = ('');
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = ("");
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = '';
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question("What is your name? ")
 }
-
+// console.log(questions[0])
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
 
+  for (let i = 0; i < questions.length; i++) {
+  candidateAnswers = input.question(questions[i])
+    if (candidateAnswers === correctAnswers[i]) {
+      console.log(`You said ${candidateAnswers} and the correct answer was ${correctAnswers[i]}. Congrats!`);
+      } else {
+        (console.log(`You said ${candidateAnswers} and the correct answer was ${correctAnswers[i]}. Try again!`));
+      }
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
+/*if (candidateAnswer === correctAnswer) {
   console.log("That's correct!");
 }   else {
       console.log("That's incorrect.");
-    }
+}*/
 
   let grade;
   
