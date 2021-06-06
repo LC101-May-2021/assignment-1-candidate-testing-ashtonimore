b1f6146d50a1cef9b89dab9581a4d4df0ad8259c
ashtonimore <>
2021-06-06 05:05:57 +0000

    MOVED THE DANG 1)-5) FOR THE AUTO GRADER

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = ('');
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = ("");
-let questions = [ '1) Who was the first American woman in space? ', '2) True or false: 5 kilometer == 5000 meters? ', '3) (5 + 3)/2 * 10 = ? ', '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', '5) What is the minimum crew size for the ISS? ' ];
+let questions = [ 'Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? ' ];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = '';
 
@@ -23,7 +23,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
   for (let i = 0; i < questions.length; i++) {
-  candidateAnswers = input.question(questions[i])
+  candidateAnswers = input.question(`${i + 1}) ${questions[i]}`)
     if (candidateAnswers === correctAnswers[i]) {
       rightAnswers = rightAnswers + 1;
       console.log(`Your answer: ${candidateAnswers} \nCorrect Answer: ${correctAnswers[i]}`);
